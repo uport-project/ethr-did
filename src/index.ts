cf7052040f3c961e23071e09e83c90c9069a84c2
Mircea Nistor <mirceanis@gmail.com>
2021-04-15 12:10:13 +0200

    feat: add support for base58 key encoding
    also removed direct dependency on 'buffer' and 'elliptic' and enforced linting during build
    
    fixes #60

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import { createJWT, verifyJWT, Signer as JWTSigner, ES256KSigner } from 'did-jwt'
+import { createJWT, verifyJWT, Signer as JWTSigner, ES256KSigner, JWTVerified } from 'did-jwt'
 import { Signer as TxSigner } from '@ethersproject/abstract-signer'
 import { CallOverrides } from '@ethersproject/contracts'
 import { computeAddress } from '@ethersproject/transactions'
+import { computePublicKey } from '@ethersproject/signing-key'
 import { getAddress } from '@ethersproject/address'
+import { Provider } from '@ethersproject/providers'
 import { Wallet } from '@ethersproject/wallet'
+import * as base64 from '@ethersproject/base64'
+import { hexlify, hexValue, isBytes } from '@ethersproject/bytes'
+import { Base58 } from '@ethersproject/basex'
+import { toUtf8Bytes } from '@ethersproject/strings'
 import { REGISTRY, EthrDidController } from 'ethr-did-resolver'
 import { Resolvable } from 'did-resolver'
-import { ec as EC } from 'elliptic'
-const secp256k1: any = new EC('secp256k1')
 
 export enum DelegateTypes {
   veriKey = 'veriKey',
@@ -17,7 +21,7 @@ export enum DelegateTypes {
 
 interface IConfig {
   identifier: string
-  chainNameOrId?: string
+  chainNameOrId?: string | number
 
   registry?: string
 
@@ -26,7 +30,8 @@ interface IConfig {
   privateKey?: string
 
   rpcUrl?: string
-  provider?: any
+  provider?: Provider
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   web3?: any
 }
 
@@ -34,6 +39,12 @@ export type KeyPair = {
   address: string
   privateKey: string
   publicKey: string
+  identifier: string
+}
+
+type DelegateOptions = {
+  delegateType?: DelegateTypes
+  expiresIn?: number
 }
 
 export class EthrDID {
@@ -45,25 +56,24 @@ export class EthrDID {
 
   constructor(conf: IConfig) {
     const { address, publicKey, network } = interpretIdentifier(conf.identifier)
-
+    const chainNameOrId = typeof conf.chainNameOrId === 'number' ? hexValue(conf.chainNameOrId) : conf.chainNameOrId
     if (conf.provider || conf.rpcUrl || conf.web3) {
       let txSigner = conf.txSigner
       if (conf.privateKey && typeof txSigner === 'undefined') {
         txSigner = new Wallet(conf.privateKey)
       }
-
       this.controller = new EthrDidController(
         conf.identifier,
         undefined,
         txSigner,
-        conf.chainNameOrId,
+        chainNameOrId,
         conf.provider || conf.web3?.currentProvider,
         conf.rpcUrl,
         conf.registry || REGISTRY
       )
       this.did = this.controller.did
     } else {
-      const net = network || conf.chainNameOrId
+      const net = network || chainNameOrId
       let networkString = net ? `${net}:` : ''
       if (networkString in ['mainnet:', '0x1:']) {
         networkString = ''
@@ -71,9 +81,7 @@ export class EthrDID {
       this.did =
         typeof publicKey === 'string' ? `did:ethr:${networkString}${publicKey}` : `did:ethr:${networkString}${address}`
     }
-
     this.address = address
-
     if (conf.signer) {
       this.signer = conf.signer
     } else if (conf.privateKey) {
@@ -81,12 +89,14 @@ export class EthrDID {
     }
   }
 
-  static createKeyPair(): KeyPair {
-    const kp = secp256k1.genKeyPair()
-    const privateKey = kp.getPrivate('hex')
-    const publicKey = '0x' + kp.getPublic(true, 'hex')
-    const address = computeAddress(publicKey)
-    return { address, privateKey, publicKey }
+  static createKeyPair(chainNameOrId?: string | number): KeyPair {
+    const wallet = Wallet.createRandom()
+    const privateKey = wallet.privateKey
+    const address = computeAddress(privateKey)
+    const publicKey = computePublicKey(privateKey, true)
+    const net = typeof chainNameOrId === 'number' ? hexValue(chainNameOrId) : chainNameOrId
+    const identifier = net ? `did:ethr:${net}:${publicKey}` : publicKey
+    return { address, privateKey, publicKey, identifier }
   }
 
   async lookupOwner(cache = true): Promise<string> {
@@ -113,14 +123,19 @@ export class EthrDID {
 
   async addDelegate(
     delegate: string,
-    { delegateType = DelegateTypes.veriKey, expiresIn = 86400 },
+    delegateOptions?: DelegateOptions,
     txOptions: CallOverrides = {}
   ): Promise<string> {
     if (typeof this.controller === 'undefined') {
       throw new Error('a web3 provider configuration is needed for network operations')
     }
     const owner = await this.lookupOwner()
-    const receipt = await this.controller.addDelegate(delegateType, delegate, expiresIn, { ...txOptions, from: owner })
+    const receipt = await this.controller.addDelegate(
+      delegateOptions?.delegateType || DelegateTypes.veriKey,
+      delegate,
+      delegateOptions?.expiresIn || 86400,
+      { ...txOptions, from: owner }
+    )
     return receipt.transactionHash
   }
 
@@ -180,10 +195,7 @@ export class EthrDID {
   async createSigningDelegate(
     delegateType = DelegateTypes.veriKey,
     expiresIn = 86400
-  ): Promise<{
-    kp: KeyPair
-    txHash: string
-  }> {
+  ): Promise<{ kp: KeyPair; txHash: string }> {
     const kp = EthrDID.createKeyPair()
     this.signer = ES256KSigner(kp.privateKey, true)
     const txHash = await this.addDelegate(kp.address, {
@@ -193,6 +205,7 @@ export class EthrDID {
     return { kp, txHash }
   }
 
+  // eslint-disable-next-line
   async signJWT(payload: any, expiresIn?: number): Promise<string> {
     if (typeof this.signer !== 'function') {
       throw new Error('No signer configured')
@@ -202,11 +215,12 @@ export class EthrDID {
       alg: 'ES256K-R',
       issuer: this.did,
     }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if (expiresIn) (<any>options)['expiresIn'] = expiresIn
     return createJWT(payload, options)
   }
 
-  async verifyJWT(jwt: string, resolver: Resolvable, audience = this.did): Promise<any> {
+  async verifyJWT(jwt: string, resolver: Resolvable, audience = this.did): Promise<JWTVerified> {
     return verifyJWT(jwt, { resolver, audience })
   }
 }
@@ -217,8 +231,8 @@ function interpretIdentifier(identifier: string): { address: string; publicKey?:
   if (input.startsWith('did:ethr')) {
     const components = input.split(':')
     input = components[components.length - 1]
-    if (components.length === 4) {
-      network = components[2]
+    if (components.length >= 4) {
+      network = components.splice(2, components.length - 3).join(':')
     }
   }
   if (input.length > 42) {
@@ -229,19 +243,22 @@ function interpretIdentifier(identifier: string): { address: string; publicKey?:
 }
 
 function attributeToHex(key: string, value: string | Uint8Array): string {
-  if (Buffer.isBuffer(value)) {
-    return `0x${value.toString('hex')}`
+  if (value instanceof Uint8Array || isBytes(value)) {
+    return hexlify(value)
   }
-  const match = key.match(/^did\/(pub|auth|svc)\/(\w+)(\/(\w+))?(\/(\w+))?$/)
-  if (match) {
-    const encoding = match[6]
-    // TODO add support for base58
+  const matchKeyWithEncoding = key.match(/^did\/(pub|auth|svc)\/(\w+)(\/(\w+))?(\/(\w+))?$/)
+  const encoding = matchKeyWithEncoding?.[6]
+  const matchHexString = (<string>value).match(/^0x[0-9a-fA-F]*$/)
+  if (encoding && !matchHexString) {
     if (encoding === 'base64') {
-      return `0x${Buffer.from(<string>value, 'base64').toString('hex')}`
+      return hexlify(base64.decode(value))
     }
-  }
-  if ((<string>value).match(/^0x[0-9a-fA-F]*$/)) {
+    if (encoding === 'base58') {
+      return hexlify(Base58.decode(value))
+    }
+  } else if (matchHexString) {
     return <string>value
   }
-  return `0x${Buffer.from(value).toString('hex')}`
+
+  return hexlify(toUtf8Bytes(value))
 }
