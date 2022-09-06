992e062229b257f653d1986d162763e31b103b7b
Philipp Bolte <philipp.bolte@spherity.com>
2022-09-06 15:25:02 +0200

    put back the deprecated hints to non-signed methods

--- src/index.ts
+++ src/index.ts
@@ -233,6 +233,7 @@ export class EthrDID {
     key: string,
     value: string | Uint8Array,
     expiresIn = 86400,
+    /** @deprecated, please use txOptions.gasLimit */
     gasLimit?: number,
     txOptions: CallOverrides = {}
   ): Promise<string> {
@@ -278,6 +279,7 @@ export class EthrDID {
   async revokeAttribute(
     key: string,
     value: string | Uint8Array,
+    /** @deprecated, please use txOptions.gasLimit */
     gasLimit?: number,
     txOptions: CallOverrides = {}
   ): Promise<string> {
