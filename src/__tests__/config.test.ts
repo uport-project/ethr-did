536ff603dd30256cd475792f7f25646880d3de21
Dennis von der Bey <dennis@vonderbey.eu>
2023-09-25 10:45:33 +0200

    fix: bigint usage in tests

--- src/__tests__/config.test.ts
+++ src/__tests__/config.test.ts
@@ -5,7 +5,7 @@ describe('configuration', () => {
     const ethrDid = new EthrDID({
       identifier: '0xC662e6c5F91B9FcD22D7FcafC80Cf8b640aed247',
       rpcUrl: 'http://localhost:9585',
-      chainNameOrId: 1337,
+      chainNameOrId: BigInt(1337),
     })
     expect(ethrDid).toBeDefined()
   })
--- src/__tests__/multinet.test.ts
+++ src/__tests__/multinet.test.ts
@@ -62,7 +62,7 @@ describe('other networks', () => {
   it('supports numbered chainId', () => {
     const ethrDid = new EthrDID({
       identifier: '0x02b97c30de767f084ce3080168ee293053ba33b235d7116a3263d29f1450936b71',
-      chainNameOrId: 42,
+      chainNameOrId: BigInt(42),
     })
     expect(ethrDid.did).toEqual('did:ethr:0x2a:0x02b97c30de767f084ce3080168ee293053ba33b235d7116a3263d29f1450936b71')
     expect(ethrDid.address).toEqual('0xC662e6c5F91B9FcD22D7FcafC80Cf8b640aed247')
